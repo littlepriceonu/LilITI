a5bfcb4d20a6302b2f8c6dc0d756ffe455c2083c
Littlepriceonu <littlepriceonu@icloud.com>
2023-08-24 23:23:43 -0700

    remove stupidity

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 #![allow(dead_code)]
 
-use std::io;
-
 // Todo:
 // * Playlist typing in SongInfo
 // * Maybe album typing?
 // * Extra stuff in the `SongInfo` struct, like if its downloaded, year and stuff like that
+//! * Implement a function in PlayerControls that lets you get *JUST* the duration and/or progress of the current song 
 
 mod itunes_interface {
     use powershell_script::{PsScriptBuilder, PsScript};
@@ -15,18 +14,20 @@ mod itunes_interface {
     //#region -----------------         TYPINGS       --------------------------
 
     pub struct SongInfo {
-        /// Name of the currently playing song
-        name: String,
+        /// Name of the currently plalbuaying song
+        pub name: String,
         /// Length (in seconds) of the currently playing song
-        duration: u8,
-        /// Formated version of the `duration` property in a String format.
-        time: String,
+        pub duration: u16,
+        /// Formated (M:S) version of the `duration` property in a String format
+        pub time: String,
         /// Progress (in seconds) of the currently playing song
-        progress: u8,
+        pub progress: u8,
+        /// Formatted (M:S) version of the `progress` property in a String format
+        pub formatted_progress: String,
         /// Album Name of the currently playing song
-        album: String,
+        pub album: String,
         /// Name of the Artist(s) that created the currently playing song
-        artist: String,
+        pub artist: String,
     }
 
     //#endregion
@@ -42,26 +43,40 @@ mod itunes_interface {
             SongInterface { itunes }
         }
 
-        pub fn get_song_info(&self ) -> SongInfo {
-            if self.itunes.is_playing_song() {
+        pub fn get_song_info(&self) -> SongInfo {
+            if !self.itunes.is_song_ready() {
                 return SongInfo {
                     name: String::new(),
                     album: String::new(),
                     artist: String::new(),
                     duration: 0,
-                    time: String::new(),
-                    progress: 0
+                    time: String::from("0:0"),
+                    progress: 0,
+                    formatted_progress: String::from("0:0"),
                 }
             }
             
+            let progress = self.itunes.get_property("PlayerPosition").trim().parse::<u8>().unwrap();
+
             return SongInfo { 
                 name: self.itunes.get_property("CurrentTrack.Name"),
                 album: self.itunes.get_property("CurrentTrack.Album"),
                 artist: self.itunes.get_property("CurrentTrack.Artist"),
-                duration: self.itunes.get_property("CurrentTrack.Duration").parse::<u8>().unwrap(),
+                duration: self.itunes.get_property("CurrentTrack.Duration").trim().parse::<u16>().unwrap(),
                 time: self.itunes.get_property("CurrentTrack.Time"),
-                progress: self.itunes.get_property("PlayerPosition").parse::<u8>().unwrap()
+                progress,
+                formatted_progress: self.format_progress(progress)
             };
+        }
+
+        pub fn format_progress(&self, progress: u8) -> String {
+            let mut x = progress/60;
+            // remove da decimal
+            x = format!("{:.0}", x).parse().unwrap();
+            
+            let y = progress - x * 60;
+
+            format!("{}:{}", x,y)
         }
     }
 
@@ -72,6 +87,7 @@ mod itunes_interface {
     pub struct ItunesPlayerControls<'t> {
         pub(crate) itunes: &'t Itunes<'t>,
         pub(crate) muted: bool,
+        pub(crate) song_interface: SongInterface<'t>,
     }
 
     impl<'t> ItunesPlayerControls<'t> {
@@ -85,6 +101,7 @@ mod itunes_interface {
 
                     return false;
                 })(),
+                song_interface: SongInterface { itunes: &itunes }
             }
         }
     
@@ -123,6 +140,14 @@ mod itunes_interface {
             }
 
             self.itunes.property("Mute = $True");
+        }
+
+        /// Checks if the currently ready song is playing 
+        pub fn is_playing(&self) -> bool {
+            match self.itunes.get_property("PlayerState").trim() {
+                "1" => true,
+                _ => false,
+            } 
         }
     }
     
@@ -167,7 +192,7 @@ mod itunes_interface {
             self.power_shell.run(&self.compile_script(prop, false)).expect("Property To Execute");
         }
         
-        pub fn is_playing_song(&self) -> bool {
+        pub fn is_song_ready(&self) -> bool {
             let current_track = self.get_property("CurrentTrack");
 
             if current_track == "" {
@@ -182,27 +207,19 @@ mod itunes_interface {
 
 }
 
+// a little example
+
 fn main() {
     let itunes = itunes_interface::Itunes::new();
     let player_controls = itunes_interface::ItunesPlayerControls::new(&itunes);
 
-    println!("Is Song Ready: {}", itunes.is_playing_song());
-    println!("Detected Volume: {}", player_controls.get_volume());
-
-    player_controls.increase_volume(25);
-
-    println!("Volume Increased By 25, Volume Is Now: {}", player_controls.get_volume());
-
-    loop {
-        let mut input = String::from("");
-
-        io::stdin()
-        .read_line(&mut input)
-        .expect("Error reading input!");
-
-        match input.trim() {
-            "play" => player_controls.play(),
-            _ => player_controls.pause()
-        }
+    if !itunes.is_song_ready() {
+        println!("You're not listening to a song!");
+        return;
     }
+
+    let song_info = player_controls.song_interface.get_song_info();
+
+    println!("\nYou're listening to {} by {}", song_info.name, song_info.artist);
+    println!("{} -- {}", song_info.formatted_progress, song_info.time);
 }
